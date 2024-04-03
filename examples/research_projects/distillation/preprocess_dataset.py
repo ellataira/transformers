365c48e9783e52acf80e1d64f6ae394b523cb9cb
ellataira <ejtaira177@gmail.com>
2024-04-03 12:32:02 -0400

    preprocess dataset -- one sentence per line , 10% ds c4

---
+++ examples/research_projects/distillation/preprocess_dataset.py
@@ -0,0 +1,39 @@
+import datasets
+import re
+import random
+
+dataset = datasets.load_dataset("allenai/c4", "en", split="train", streaming=True, cache_dir="../", download_config=datasets.DownloadConfig(cache_dir="../"))
+
+# Create a text file to store the compiled text
+text_file = open("/scratch/taira.e/c4_10_dataset_distill.txt", "w", encoding="utf-8")
+sentence_pattern = re.compile(r'[.!?]|[\n]')
+
+"""
+Define the size of the subset you want to create
+c4 en train set = 364868892 = 305 gb
+"""
+
+subset_size = 36000000 # ~ 10% of corpus
+
+# Iterate over the dataset and write the text to the file
+for i, example in enumerate(dataset):
+    if i > subset_size :
+        break
+
+    # sentences = [sentence.strip() for sentence in re.split(sentence_pattern, example['text']) if sentence.strip()]
+    #
+    # # Write each sentence to the file, with each sentence duplicated and separated by a tab
+    # for sentence in sentences:
+    #     text_file.write(f"{sentence}\n")
+    for s in re.split(sentence_pattern, example['text']):
+        if s.strip():
+            to_add = s.strip()
+            text_file.write(f"{to_add}\n")
+
+# Close the file
+text_file.close()
+print("Wrote dataset of size: ", subset_size)
+
+
+
+
---
+++ examples/research_projects/distillation/preprocess_dataset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=08:00:00
+#SBATCH --job-name=preproc_c4
+#SBATCH --mem=100G
+#SBATCH --ntasks=1
+#SBATCH --output=logs/myjob.%j.out
+#SBATCH --error=logs/myjob.%j.err
+
+module load anaconda3/2022.05 cuda/12.1
+conda create --name pytorch_env python=3.9 -y
+conda activate pytorch_env
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+python -c'import torch; print(torch.cuda.is_available())'
+
+
+# Install required Python packages
+pip install datasets bert-pytorch
+#pip uninstall codecarbon
+
+# # Run your Python script
+# python preprocess_dataset.py
+# Run your Python script in the background
+python preprocess_dataset.py
+
+# Deactivate the virtual environment
+deactivate
+
+# TODO : output file?
+# sbatch dataset --- add nvidia-smi query
+# carbon tracking
+# GPU: nvidia-smi --query-gpu=power.draw --format=csv --filename=/home/taira.e/power_stats/gpu_test.csv
+
