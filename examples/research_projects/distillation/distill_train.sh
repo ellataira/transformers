45467564ae0a4867b314d69060e17e099cc98315
ellataira <ejtaira177@gmail.com>
2024-04-08 17:30:45 -0400

    preconfig conda env

--- examples/research_projects/distillation/distill_train.sh
+++ examples/research_projects/distillation/distill_train.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=:v100-sxm2:1
 #SBATCH --time=08:00:00
-#SBATCH --job-name=distilltrain4.7
+#SBATCH --job-name=distilltrain4.8
 #SBATCH --mem=200G
 #SBATCH --ntasks=1
-#SBATCH --output=logs/distilltrain47.%j.out
-#SBATCH --error=logs/distilltrain47.%j.err
+#SBATCH --output=logs/distilltrain48.%j.out
+#SBATCH --error=logs/distilltrain48.%j.err
 
 module load anaconda3/2022.05 cuda/12.1
-conda create --name pytorch_env python=3.9 -y
-conda activate pytorch_env
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+#conda create --name pytorch_env python=3.9 -y
+source activate greenai
+#conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 python -c'import torch; print(torch.cuda.is_available())'
 
 #cd /home/taira.e/transformers/examples/research_projects/distillation
@@ -46,7 +46,7 @@ while ps -p $distill_pid > /dev/null; do
 
     # Get GPU power draw and append to CSV file
     power_draw=$(nvidia-smi --query-gpu=power.draw --format=csv,noheader,nounits)
-    echo "$timestamp,$power_draw" >> /home/taira.e/power_stats/distill_train4_6.csv
+    echo "$timestamp,$power_draw" >> /home/taira.e/power_stats/distill_train4_8.csv
 
     sleep 600  # 10 mins
 
@@ -54,5 +54,5 @@ done
 
 wait $distill_pid
 
-deactivate
+conda deactivate
 
